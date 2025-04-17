2ea7ecadc1bc46c6c488501efb32dfc3dac6f6b6
DivyanshSharmaji <divyansh04sharmads@gmail.com>
2025-04-17 16:57:03 +0530

    Same Image Added + Source File Modified

--- Receiver.py
+++ Receiver.py
@@ -1,39 +1,51 @@
 import socket
 import Receiver_Key
+import time
 
 c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # By default it will use IPV4 and TCP for connection
 
 c_sock.connect(('10.1.3.250',9999))
+# c_sock.connect(('10.1.3.250',9999))
 
 e,d,n = Receiver_Key.get_keys()
+print("Generated e and n (Public key) as follows: ")
+print("e = ",e," n = ",n)
+print("Sent values of e and n from receiver end")
+time.sleep(5)
 c_sock.send(bytes(str(e),'utf-8'))
+time.sleep(0.1)
 c_sock.send(bytes(str(n),'utf-8'))
 
-ciphertext=[]
-while True:
-    ch = c_sock.recv(1024).decode()
-    if ch:
-        ciphertext.append(int(ch))
-    else:
-        break
 
-# encrypted_msg = b""
-# for ch in ciphertext:
-#     encrypted_msg += bytes(str(ch),'utf-8')
+time.sleep(5)
+received_bytes = c_sock.recv(409600)
+ciphertext = []
+for i in range(0, len(received_bytes), 4):  # 4 bytes per int
+    chunk = received_bytes[i:i+4]
+    if len(chunk) == 4:
+        num = int.from_bytes(chunk, byteorder='big')
+        ciphertext.append(num)
+print("File data(Encrypted integer list): ",ciphertext)
+time.sleep(5)
 
-# with open('Encrypted_d.jpg','wb') as file:
-#     file.write(encrypted_msg)
-# file.close()
+
+encrypted_msg = b''
+for c in ciphertext:
+    encrypted_msg += c.to_bytes(4, byteorder='big')
+with open('Encrypted_d.jpg','wb') as file:
+    file.write(encrypted_msg)
+file.close()
 
 decoded_msg = [pow(int(c), d, n) for c in ciphertext]
+print("File data(integer): ",decoded_msg)
+time.sleep(5)
 
-# message = b""
-# for ch in decoded_msg:
-#     message += bytes(str(ch),'utf-8')
-
+message = bytes(decoded_msg)
+print("File data(Binary): ",message)
+time.sleep(5)
 with open('Decrypted_d.jpg','wb') as file:
-    # file.write(message)
-    
+    file.write(message)
+print("File successfully decrypted")
 
 file.close()
 
--- Sender.py
+++ Sender.py
@@ -8,22 +8,37 @@ sock.bind(('10.1.3.250',9999)) # Binds the socket with a port number(0-65535), l
 
 sock.listen(5)
 print('Waiting for connection...')
+
 c_sock, addr = sock.accept() # Accepts the connection from the client and returns client socket and address of the client
 print("Connection established with ",addr)
+
 e = c_sock.recv(1024).decode() # Public key of receiver
+time.sleep(0.01)
 n = c_sock.recv(1024).decode()
+print("Received public key(e,n) of receiver as follows:")
+print("e = ",e," n = ",n)
+time.sleep(5)
 
 msg = b""
 with open('d.jpg','rb') as file:
     msg = file.read()
+print("File data(Binary): ",msg)
+time.sleep(5)
 
 integer_list = list(msg)
+print("File data(integer): ",integer_list)
+time.sleep(5)
+
 ciphertext = [pow(ch, int(e), int(n)) for ch in integer_list]
 # pow(ch,e,n) = (ch^e)%n
+print("File data(Encrypted integer list): ",ciphertext)
+time.sleep(5)
 
-for ch in ciphertext:
-    c_sock.send(bytes(str(ch),'utf-8'))
-    time.sleep(0.000001)
+byte_data = b''
+for c in ciphertext:
+    # Send each integer in 4 bytes (assuming c < 2^32), change if needed
+    byte_data += c.to_bytes(4, byteorder='big')
+c_sock.sendall(byte_data)
 print("Successfully sent the encrypted file")
 
 c_sock.close()
